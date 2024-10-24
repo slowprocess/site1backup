249ca03d0bc6ff9e302b4a325ef7e0e3524dc026
slowprocess <78941527+slowprocess@users.noreply.github.com>
2024-10-24 22:31:45 +0100

    Initial commit

---
+++ Main/1_files/1.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20230106
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('1_files/1Moz.css')
+detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
